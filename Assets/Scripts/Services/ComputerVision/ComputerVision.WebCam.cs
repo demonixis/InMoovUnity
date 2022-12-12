165ac4b49937077334e53dc18b5841a6292a1bc1
Yannick <comte.yannick@gmail.com>
2022-12-12 19:06:45 +0100

    Added the first step of the Computer Vision service with initial WebCam support

---
+++ Assets/Scripts/Services/ComputerVision/ComputerVision.WebCam.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+
+namespace Demonixis.InMoov.ComputerVision
+{
+    public partial class ComputerVisionService
+    {
+        private WebCamTexture[] _webCamTextures;
+        
+        public void SetWebCamTextureEnabled(bool left, bool start)
+        {
+            if (_webCamTextures == null ||
+                left && _webCamTextures[0] == null ||
+                !left && _webCamTextures[1] == null)
+                return;
+
+            var texture = _webCamTextures[left ? 0 : 1];
+
+            if (start)
+                texture.Play();
+            else
+                texture.Stop();
+        }
+
+        public WebCamTexture GetWebCamTexture(bool left)
+        {
+            return _webCamTextures[left ? 0 : 1];
+        }
+
+        public void InitializeWebCamTexture(int cameraIndex, bool left, bool autoPlay = false)
+        {
+            if (!TryGetWebcamInfos(cameraIndex, out string cameraName, out int width, out int height))
+            {
+                Debug.LogError($"Can't open the webcam {cameraIndex}");
+                return;
+            }
+
+            var texture = new WebCamTexture(cameraName, width, height);
+            if (texture == null)
+            {
+                Debug.LogError($"Can't create a webcam texture for the camera {cameraIndex}");
+                return;
+            }
+
+            if (_webCamTextures == null)
+                _webCamTextures = new WebCamTexture[2];
+
+            _webCamTextures[left ? 0 : 1] = texture;
+        }
+
+        private bool TryGetWebcamInfos(int cameraIndex, out string cameraName, out int width, out int height)
+        {
+            cameraName = string.Empty;
+            width = 0;
+            height = 0;
+
+            var devices = WebCamTexture.devices;
+
+            if (devices.Length == 0 || devices.Length < cameraIndex) return false;
+
+            var camera = devices[cameraIndex];
+            var resolution = GetBestResolution(camera.availableResolutions);
+
+            cameraName = camera.name;
+            width = resolution.width;
+            height = resolution.height;
+
+            return true;
+        }
+
+        private Resolution GetBestResolution(Resolution[] resolutions)
+        {
+            if (resolutions == null || resolutions.Length == 0)
+            {
+                return new Resolution
+                {
+                    width = 640,
+                    height = 480
+                };
+            }
+
+            var bestResolution = new Resolution();
+
+            foreach (var res in resolutions)
+            {
+                if (res.width <= bestResolution.width || res.height <= bestResolution.height) continue;
+                bestResolution.width = res.width;
+                bestResolution.height = res.height;
+            }
+
+            return bestResolution;
+        }
+    }
+}
--- Assets/Scripts/Services/ComputerVision/ComputerVisionService.cs
+++ Assets/Scripts/Services/ComputerVision/ComputerVisionService.cs
@@ -1,9 +1,43 @@
-﻿namespace Demonixis.InMoov.ComputerVision
+﻿using Demonixis.InMoov.Settings;
+
+namespace Demonixis.InMoov.ComputerVision
 {
-    public class ComputerVisionService : RobotService
+    public partial class ComputerVisionService : RobotService
     {
+        private const string CVFilename = "computer-vision.json";
+        private CVSettings _settings;
+
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            _settings = SaveGame.LoadRawData<CVSettings>(SaveGame.GetPreferredStorageMode(), CVFilename, "Config");
+
+            if (!_settings.IsValid())
+                return;
+
+            if (_settings.IsCameraConnected(true))
+                InitializeWebCamTexture(_settings.WebCamLeft, true);
+
+            if (_settings.IsCameraConnected(false))
+                InitializeWebCamTexture(_settings.WebCamRight, false);
+        }
+
         public override void SetPaused(bool paused)
         {
+        }
+
+        public override void Shutdown()
+        {
+            base.Shutdown();
+
+            if (_webCamTextures == null) return;
+
+            foreach (var texture in _webCamTextures)
+            {
+                if (texture != null)
+                    texture.Stop();
+            }
         }
     }
 }
